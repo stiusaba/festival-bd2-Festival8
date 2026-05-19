f40cb7a1b2cb380cc7bdc7f6e8e894a4c4b7d2a5
SebastianB007 <132923535+SebastianB007@users.noreply.github.com>
2026-05-19 17:42:29 -0500

    Creación inicial de colecciones MongoDB

---
+++ scripts_mongodb/creacion_colecciones.js
@@ -0,0 +1,9 @@
+use("soundwave_mongodb");
+
+db.createCollection("setlists");
+
+db.createCollection("riders_tecnicos");
+
+db.createCollection("resenas_publico");
+
+db.createCollection("reportes_incidentes");
